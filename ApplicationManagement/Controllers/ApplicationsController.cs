901d74827173bd71073487d2fd29fff65436a6e1
Gabriel Schenker <gnschenker@gmail.com>
2019-11-08 17:03:02 +0100

    First commit

---
+++ ApplicationManagement/Controllers/ApplicationsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApplicationManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Application 1", "Application 2", "Application3", "Application 4" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Application {id}";
+        }
+    }
+}
---
+++ ProgramSelection/Controllers/ProgramsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProgramSelection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgramsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Program 1", "Program 2", "Program 3" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return $"Program {id}";
+        }
+    }
+}
